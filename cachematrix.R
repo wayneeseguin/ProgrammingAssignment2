3f4f1ed1821d5fdab60f3f290494e0457f6b2a26
Wayne E. Seguin <wayneeseguin@gmail.com>
2014-06-22 13:26:12 -0400

    Implemented makeCacheMatrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##
+## Function: makeCacheMatrix
+## 
+## Creates a special CacheMatrix object which can store the results of a
+## matrix inverse calculation so it only has to be computed once.
+## 
+## Parameters: A Matrix
+##
+## Returns: a list of functions defined on the given CacheMatrix
+##
 makeCacheMatrix <- function(x = matrix()) {
+    # Define CacheMatrix Function to set the object and inverse cache
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
 
+    # Define CacheMatrix function to set the inverse in the cache
+    setInverse <- function(inv) inverse <<- inv
+
+    # Define CacheMatrix function to get the cached inverse
+    getInverse <- function(inverse)
+
+    # Define CacheMatrix function to tell if a cached inverse exists
+    isCached <- function() !is.null(inverse)
+
+    # Cache the given matrix object
+    set(x)
+
+    # Return the list of defined functions on the new CacheMatrix object
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse,
+         isCached = isCached
+        )
 }
 
-
-## Write a short comment describing this function
-
+##
+## Function: cacheSolve
+##
+## Computes the inverse of a matrix and stores it in the special 
+## CacheMatrix's cache, if it has not already been computed 
+##
+## Parameters: An (assumed) Invertable Matrix.
+##
+## Returns: A matrix which is the inverse of the original
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # If the computation is not cached,
+    if(!x$isCached()) {
+        # Perform the computation
+        inv <- solve(x$get(),...)
+
+        # and set the results to the CacheMatrix's cache.
+        x$setInverse()
+    }
+
+    # Return the inverse from the cache.
+    x$getInverse()
 }
